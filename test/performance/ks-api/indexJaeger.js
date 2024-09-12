1d44386251f7c0733606df1cac4fd7a38c2a30de
José María García <josemgarcia@us.es>
2024-09-12 16:53:40 +0200

    Jaeger test case results

---
+++ test/performance/ks-api/indexJaeger.js
@@ -0,0 +1,66 @@
+'use strict';
+
+// must be in the first two lines of your application
+const Tracer = require('@risingstack/jaeger')
+const tracer = new Tracer({
+  serviceName: 'ks-api'
+})
+
+var fs = require('fs'),
+  http = require('http'),
+  path = require('path');
+
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json({
+  strict: false,
+  limit: '50mb'
+}));
+
+
+var oasTools = require('oas-tools');
+var jsyaml = require('js-yaml');
+var serverPort = process.env.PORT || 8080;
+
+var spec = fs.readFileSync(path.join(__dirname, '/api/oas-doc.yaml'), 'utf8');
+var oasDoc = jsyaml.safeLoad(spec);
+
+var options_object = {
+  controllers: path.join(__dirname, './controllers'),
+  loglevel: 'error',
+  strict: false,
+  router: true,
+  validator: true
+};
+
+oasTools.configure(options_object);
+
+oasTools.initialize(oasDoc, app, function () {
+  http.createServer(app).listen(serverPort, function () {
+    console.log("App running at http://localhost:" + serverPort);
+    console.log("________________________________________________________________");
+    if (options_object.docs !== false) {
+      console.log('API docs (Swagger UI) available on http://localhost:' + serverPort + '/docs');
+      console.log("________________________________________________________________");
+    }
+  });
+});
+
+app.get('/', function (req, res) {
+  res.redirect('/docs');
+});
+
+const v8 = require('v8');
+app.get('/heapStats', function (req, res) {
+  var heapStats = v8.getHeapStatistics();
+
+  // Round stats to MB
+  var roundedHeapStats = Object.getOwnPropertyNames(heapStats).reduce(function (map, stat) {
+    map[stat] = Math.round((heapStats[stat] / 1024 / 1024) * 1000) / 1000;
+    return map;
+  }, {});
+  roundedHeapStats['units'] = 'MB';
+
+  res.send(roundedHeapStats);
+});
