a2b092cb02c4f0b23d0e48552dacbc412debd979
motero2k <manolo.ob@hotmail.com>
2025-06-19 11:56:27 +0100

    mini fix, logs page not ready yet

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import MetricsPage from "./pages/metrics/MetricsPage";
 import TracesPage from "./pages/traces/TracesPage";
-import LogsPage from "./pages/logs/LogsPage";
 import TracesDetailPage from "./pages/traces/TracesDetailPage";
 import WorkInProgressPage from "./pages/WorkInProgressPage";
 import NotFoundPage from "./pages/NotFoundPage";
@@ -30,7 +29,7 @@ function App() {
         <Route path="/metrics" element={<PageTemplate activeTab="metrics"><MetricsPage /></PageTemplate>} />
         <Route path="/traces" element={<PageTemplate activeTab="traces"><TracesPage /></PageTemplate>} />
         <Route path="/traces/details" element={<PageTemplate activeTab="traces"><TracesDetailPage /></PageTemplate>} />
-        <Route path="/logs" element={<PageTemplate activeTab="logs"><LogsPage /></PageTemplate>} />
+        <Route path="/logs" element={<PageTemplate activeTab="logs"><WorkInProgressPage /></PageTemplate>} />
         <Route path="/plugins" element={<PageTemplate activeTab="plugins"><WorkInProgressPage /></PageTemplate>} />
         <Route path="*" element={<PageTemplate activeTab=""><NotFoundPage /></PageTemplate>} />
       </Routes>
